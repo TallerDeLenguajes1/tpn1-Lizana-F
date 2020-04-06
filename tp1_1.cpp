be3f257bc2f2fe482c4f378c7bbda7e69cb80579
Lizana Franco <franck_l_m@hotmail.com.ar>
2020-04-06 15:28:09 -0300

    ignore agregado , tp1 creado

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main(){
-
+	int numero;
+	int *punteroNumero;
+	numero = 4;
+	punteroNumero=&numero;
+	
+	printf("contenido del puntero =%d\n",*punteroNumero);
+	printf("direccion almacenada en el puntero=%p\n",*punteroNumero);
+	printf("direccion de la variable (numero)=%p\n",numero);
+	printf("direccion del puntero =%p \n",punteroNumero);
+	printf("longitud de memoria de la variable=%d\n",sizeof(numero));
+	
 
     return 0;
 }   
